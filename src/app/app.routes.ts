8e11b00d92900423a1c955becb860e9e38c1b976
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2025-07-10 10:30:58 -0300

    - Implementa conteúdo do interface do componente login-form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./auth/pages/login/login').then(m => m.Login)
+    },
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    }
+];
