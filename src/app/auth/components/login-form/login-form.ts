a613aba2440bfea49bbee21b94924f628043e42e
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2025-07-11 15:46:08 -0300

    Implementa reactiveForms e Validators para o component loginForm

--- src/app/auth/components/login-form/login-form.ts
+++ src/app/auth/components/login-form/login-form.ts
@@ -1,11 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login-form.html',
-  styleUrl: './login-form.scss'
+  styleUrls: ['./login-form.scss']
 })
 export class LoginForm {
+  public loginForm: FormGroup
+  constructor(private _formBuilder: FormBuilder) {
+    this.loginForm = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
 
+  get email() {
+    return this.loginForm.get('email')!;
+  }
+
+  get password(){
+    return this.loginForm.get('password')!;
+  }
+
+  onSubmit(){
+    console.log(this.loginForm.valid);
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value)
+    }
+  }
 }
+
