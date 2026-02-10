73c486425e301d6c42b492edad8e905c00d78a5a
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-02-10 18:38:24 -0300

    feat(domain): add user entity agregate root

---
+++ src/Cashly.Domain/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using Cashly.Domain.Entities.Bases;
+using Cashly.Domain.ValueObjects;
+
+namespace Cashly.Domain.Entities
+{
+    public sealed class User : Entity
+    {
+        public Name Name { get;  private set; } = null!;
+        public Email Email { get; private set; } = null!;
+        public PasswordHash PasswordHash { get; private set; } = null!;
+        public DateTimeOffset CreatedAt { get; private set; }
+        public DateTimeOffset UpdatedAt { get; private set; }
+
+        private User(Guid id, Name name, Email email, PasswordHash passwordHash) : base(id)
+        {
+            Name = name;
+            Email = email;
+            PasswordHash = passwordHash;
+            CreatedAt = DateTimeOffset.UtcNow;
+            UpdatedAt = DateTimeOffset.UtcNow;
+        }
+
+        private User() { }
+
+        public static User Create(Name name, Email email, PasswordHash passwordHash)
+            => new User(Guid.NewGuid(), name, email, passwordHash);
+        
+    }
+}
