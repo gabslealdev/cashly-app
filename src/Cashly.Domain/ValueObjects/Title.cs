bab2112d20d534484e75ef95f0e060f990eae4ba
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-02-10 15:16:03 -0300

    feat(Domain): Add title value object

---
+++ src/Cashly.Domain/ValueObjects/Title.cs
@@ -0,0 +1,30 @@
+﻿using Cashly.Domain.Exceptions;
+
+namespace Cashly.Domain.ValueObjects
+{
+    public sealed record Title
+    {
+        public string Value { get; private set; }
+
+        public Title(string value)
+        {
+            Value = value;
+        }
+        private Title() { }
+
+        public static Title Create(string value)
+        {
+            value = Normalize(value);
+            Validate(value);
+            return new Title(value);
+        }
+
+        private static string Normalize(string value) => value?.Trim() ?? string.Empty;
+
+        private static void Validate(string value)
+        {
+            DomainExceptionValidation.When(string.IsNullOrWhiteSpace(value), "Title is required");
+            DomainExceptionValidation.When(value.Length < 3, "Title must be at least 3 characters long");
+        }
+    }
+}
