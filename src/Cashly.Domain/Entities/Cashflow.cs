d6c49e0ce8ec7620dd190495beb42d9d8d5b7040
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-02-10 18:54:22 -0300

    feat(domain): add cashflow and core attributes

---
+++ src/Cashly.Domain/Entities/Cashflow.cs
@@ -0,0 +1,26 @@
+﻿using Cashly.Domain.Entities.Bases;
+using Cashly.Domain.ValueObjects;
+
+namespace Cashly.Domain.Entities
+{
+    public sealed class Cashflow : Entity
+    {
+
+        public Title Title { get; private set; } = null!;
+        public DateTimeOffset CreateadAt { get; private set; }
+        public DateTimeOffset UpdateadAt { get; private set; }
+
+        private Cashflow(Guid id, Title title) : base(id)
+        {
+            Title = title;
+            CreateadAt = DateTimeOffset.UtcNow;
+            UpdateadAt = DateTimeOffset.UtcNow;
+        }
+
+        private Cashflow() { }
+
+        public static Cashflow Create(Title title)
+            => new Cashflow(Guid.NewGuid(), title);
+
+    }
+}
