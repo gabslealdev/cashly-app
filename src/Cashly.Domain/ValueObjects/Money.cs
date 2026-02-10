53a7900626b49b5ef3554053462152f050354bf6
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-02-10 15:16:22 -0300

    feat(Domain) Add money value object

---
+++ src/Cashly.Domain/ValueObjects/Money.cs
@@ -0,0 +1,30 @@
+﻿using Cashly.Domain.Exceptions;
+
+namespace Cashly.Domain.ValueObjects
+{
+    public sealed record Money
+    {
+        public decimal Value { get; private set; }
+
+        public Money(decimal value)
+        {
+            Value = value;
+        }
+
+        private Money() { }
+
+        public static Money Create(decimal value)
+        {
+            return new Money(value);
+        }
+
+
+        public static Money operator +(Money a, Money b) => new(a.Value + b.Value);
+
+        public static Money operator -(Money a, Money b) => new(a.Value - b.Value);
+
+
+        public override string ToString() => $"R$ {Value:N2}";
+
+    }
+}
