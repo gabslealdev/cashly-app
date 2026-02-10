1563b587a1a6b5810c5ab143ababd850e5a3a4ea
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-02-10 01:23:52 -0300

    feat(domain): Add Name value object

---
+++ src/Cashly.Domain/ValueObjects/Name.cs
@@ -0,0 +1,39 @@
+﻿using Cashly.Domain.Exceptions;
+
+namespace Cashly.Domain.ValueObjects
+{
+    public sealed record Name
+    {
+        public string FirstName { get; private set; } 
+        public string LastName { get; private set; }
+
+        private Name(string firstName, string Lastname)
+        {
+            FirstName = firstName;
+            LastName = Lastname;
+        }
+
+        private Name(){}
+
+        public static Name Create(string firstName, string lastName)
+        {
+            Normalize(firstName);
+            Normalize(lastName);
+            Validate(firstName, lastName);
+            return new Name(firstName, lastName);
+        }
+
+        private static string Normalize(string value) => value?.Trim() ?? string.Empty;
+
+        private static void Validate(string firstName, string lastName)
+        {
+            DomainExceptionValidation.When(string.IsNullOrWhiteSpace(firstName), "First name is required");
+            DomainExceptionValidation.When(firstName.Length < 3, "First name must be at least 3 characters long"); ;
+            DomainExceptionValidation.When(string.IsNullOrWhiteSpace(lastName), "Last name is required");
+            DomainExceptionValidation.When(lastName.Length < 3, "Last name must be at least 3 characters long");
+        }
+        
+        public override string ToString() => $"{FirstName} {LastName}";
+
+    }
+}
