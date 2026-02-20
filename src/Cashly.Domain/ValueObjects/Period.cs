e704688c94f8baae84fc00ae2777de9b893eb5d1
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-02-20 11:55:37 -0300

    refactor(Domain): implement IComparable on period to improve testability

--- src/Cashly.Domain/ValueObjects/Period.cs
+++ src/Cashly.Domain/ValueObjects/Period.cs
@@ -2,13 +2,14 @@
 
 namespace Cashly.Domain.ValueObjects
 {
-    public sealed record Period
+    public sealed record Period : IComparable<Period>
     {
         public int Year { get; private set; }
         public int Month { get; private set; }
 
         private Period(int year, int month)
         {
+            Validate(year, month);
             Year = year;
             Month = month;
         }
@@ -21,15 +22,44 @@ namespace Cashly.Domain.ValueObjects
             DomainExceptionValidation.When(month < 1 || month > 12, "Month must be between 1 and 12");
         }
 
-        public static Period FromDate(DateTimeOffset date)
+        public static Period FromDate(DateTimeOffset date) 
+            => Create(date.Year, date.Month);  
+
+        public static Period Create(int year, int month) 
+            => new Period(year, month);
+
+        public bool isFuture()
+            => this > Current();
+
+        public static Period Current()
+            => Create(DateTimeOffset.UtcNow.Year, DateTimeOffset.UtcNow.Month);
+
+         public override string ToString() 
+            => $"{Year:D4}-{Month:D2}";
+
+        public int CompareTo(Period? other)
         {
-            return Create(date.Year, date.Month); 
+            if (other is null)
+                return 1;
+
+            var yearComparsion = Year.CompareTo(other.Year);
+            if( yearComparsion != 0 )
+                return yearComparsion;
+
+            return Month.CompareTo(other.Month);
         }
 
-        public static Period Create(int year, int month)
-        {
-            return new Period(year, month);
-        }
-         public override string ToString() => $"{Year:D4}-{Month:D2}";
+        public static bool operator >(Period left, Period right)
+            => left.CompareTo(right) > 0;
+
+        public static bool operator <(Period left, Period right)
+            => left.CompareTo(right) < 0;
+
+        public static bool operator >=(Period left, Period right)
+            => left.CompareTo(right) >= 0;
+
+        public static bool operator <=(Period left, Period right)
+            => left.CompareTo(right) <= 0;
+
     }
 }
